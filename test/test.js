288766e390b2f88628f721df334799e78c9599d1
Tanakorn Phoochaliaw <tanakorn@digio.co.th>
2017-02-17 14:29:00 +0700

    Url google.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var should = require("should");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://www.apple.com");
+var server = supertest.agent("http://www.google.com");
 
 // UNIT test begin
 
